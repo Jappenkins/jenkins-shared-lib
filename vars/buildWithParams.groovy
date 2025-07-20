30686413035d9819ba2ef07822418bc288f4e9ed
Salem Almutairi <salemsnsalmutairi@gmail.com>
2025-07-20 13:46:03 +0300

    added params

--- vars/buildWithParams.groovy
+++ vars/buildWithParams.groovy
@@ -2,3 +2,23 @@ def call(String name = 'world') {
     echo "Hello, ${name}!"
 }
 
+def chooseBuildTool(String buildTool) {
+    switch (buildTool) {
+        case 'maven':
+            echo "Using Maven as build tool..."
+            break
+        case 'gradle':
+            echo "Using Gradle as build tool..."
+            break
+    }
+}
+def choosePlatform(String platform) {
+    switch (platform) {
+        case 'linux':
+            echo "Using Linux as platform..."
+            break
+        case 'windows':
+            echo "Using Windows as platform..."
+            break
+    }
+}
